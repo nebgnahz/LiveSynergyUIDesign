66284863532fddbcf643de46904629d91696d0c8
nebgnahz <nebgnahz@gmail.com>
2011-08-01 14:48:25 +0800

    whole architecture prototype

--- AddNewThing.xaml.cs
+++ AddNewThing.xaml.cs
@@ -18,6 +18,30 @@ namespace LiveSynergyUIDesign
         public AddNewThing()
         {
             InitializeComponent();
+
+
+            List<string> cities = new List<string>();
+            cities.Add("London");
+            cities.Add("Seattle");
+            cities.Add("Tokyo");
+            cities.Add("Nairobi");
+            cities.Add("Lisbon");
+            cities.Add("New York");
+            cities.Add("Paris");
+            cities.Add("San Francisco");
+            AutoComplete.ItemsSource = cities;
+
         }
+
+        void OnClickCheckNewObject(object sender, EventArgs args)
+        {
+
+        }
+        void OnClickCancelNewObject(object sender, EventArgs args)
+        {
+
+        }
+
     }
+
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,38 +9,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Microsoft.Phone.Shell;
 using Microsoft.Phone.Controls;
+
 
 namespace LiveSynergyUIDesign
 {
     public partial class MainPage : PhoneApplicationPage
     {
-
-        List<PhysicalObject> PublicThings = new List<PhysicalObject>
-        {
-            new PhysicalObject(){ ObjectName="Television" },
-            new PhysicalObject(){ ObjectName="Light" },
-            new PhysicalObject(){ ObjectName="Computer"},
-            new PhysicalObject(){ ObjectName="Xbox 360"},
-            new PhysicalObject(){ ObjectName="Oscilloscope"}
-        };
-        
-        List<PhysicalObject> PrivateThings = new List<PhysicalObject>
-        {
-            new PhysicalObject(){ ObjectName="My PC" },
-            new PhysicalObject(){ ObjectName="My light" },
-            new PhysicalObject(){ ObjectName="my laptop"},
-            new PhysicalObject(){ ObjectName="my elevator"},
-            new PhysicalObject(){ ObjectName="my cubicle"}
-        };
-        
-
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            PublicThingList.ItemsSource = PublicThings;
-            PrivateThingList.ItemsSource = PrivateThings;
+            LoadDevices();
+            ListPublicThing.SelectionChanged += OnPublicDeviceSelectedChanged;
+
+       }
+
+        public static System.Collections.Generic.IEnumerable<PublicGrouping<string, Device>> deviceByLocation;
+        public static System.Collections.Generic.IEnumerable<PublicGrouping<string, Device>> deviceByName;
+
+        private void LoadDevices()
+        {
+            List<Device> PublicThings = new List<Device>
+            {
+                new Device(){ DeviceName="Television", DeviceLocation="\\msra\\floor\\12\\focus_room" },
+                new Device(){ DeviceName="Light", DeviceLocation="\\msra\\floor\\12\\focus_room"},
+                new Device(){ DeviceName="Light2", DeviceLocation="\\msra\\floor\\12\\focus_room"},
+                new Device(){ DeviceName="Light3", DeviceLocation="\\msra\\floor\\12\\focus_room"},
+                new Device(){ DeviceName="Computer", DeviceLocation="\\msra\\floor\\12\\fred_cubical"},
+                new Device(){ DeviceName="Xbox 360", DeviceLocation="\\msra\\floor\\12\\lobby"},
+                new Device(){ DeviceName="Oscilloscope", DeviceLocation="\\msra\\floor\\12\\fred_cubical"}
+            };
+
+            deviceByLocation = from device in PublicThings
+                                   group device by device.DeviceLocation into c
+                                   orderby c.Key
+                                   select new PublicGrouping<string, Device>(c); 
+            
+            deviceByName = from device in PublicThings
+                                   group device by device.DeviceNameFirstLetter into c
+                                   orderby c.Key
+                                   select new PublicGrouping<string, Device>(c);
+
+            ListPublicThing.ItemsSource = deviceByName;
+
         }
 
         void FillItUp(ItemsControl itemsControl, string[] ItemsCollection)
@@ -55,7 +68,9 @@ namespace LiveSynergyUIDesign
             this.NavigationService.Navigate(new Uri("/AddNewThing.xaml", UriKind.Relative));
         }
         void OnAppbarDeleteClick(object sender, EventArgs args)
-        { }
+        {
+            
+        }
 
         void OnAppbarNextClick(object sender, EventArgs args)
         { }
@@ -75,5 +90,81 @@ namespace LiveSynergyUIDesign
         {
 
         }
+        void OnClickSignUp(object sender, EventArgs args)
+        {
+            this.NavigationService.Navigate(new Uri("/SignUpPage.xaml", UriKind.Relative));
+        }
+
+        void OnMenuClicked(object sender, RoutedEventArgs args)
+        {
+            string command = (string)(((MenuItem)sender).Tag);
+            switch (command)
+            {
+                case ("VIEW"): break;
+                case ("REMOVE"): break;
+                default: break;
+            }
+        }
+        /*
+            ListHeaderTemplate="{StaticResource DeviceListHeader}"
+            GroupHeaderTemplate="{StaticResource DeviceGroupHeader}"
+            GroupItemTemplate="{StaticResource groupItemHeader}"
+            ItemTemplate="{StaticResource DeviceItemTemplate}"
+         */
+        void OnClickSortByLocation(object sender, EventArgs args)
+        {
+            ListPublicThing.GroupHeaderTemplate = this.Resources["DeviceGroupHeaderLocation"] as DataTemplate;
+            ListPublicThing.ItemsSource = deviceByLocation;
+            ListPublicThing.GroupItemTemplate = this.Resources["groupItemHeaderLocation"] as DataTemplate;            
+        }
+        void OnClickSortByName(object sender, EventArgs args)
+        {
+            ListPublicThing.GroupHeaderTemplate = this.Resources["DeviceGroupHeaderNameFirst"] as DataTemplate; 
+            ListPublicThing.ItemsSource = deviceByName;
+            ListPublicThing.GroupItemTemplate = this.Resources["groupItemHeaderNameFirst"] as DataTemplate;
+        }
+        void OnPublicDeviceSelectedChanged(object sender, EventArgs args)
+        {
+            Device device = ListPublicThing.SelectedItem as Device;
+            if (device != null)
+            {
+                NavigationService.Navigate(new Uri("/ViewObjectPage.xaml?Name=" + device.DeviceName, UriKind.Relative));
+            }
+        }
+
+
+
+        void OnTapFlowChart(object sender, EventArgs args)
+        {
+            this.NavigationService.Navigate(new Uri("/Program.xaml", UriKind.Relative));
+        }
+
+
+        void OnTapEvent(object sender, EventArgs args)
+        {
+            this.NavigationService.Navigate(new Uri("/LongListSelection.xaml", UriKind.Relative));
+        }
+
+        void OnTapCommand1(object sender, EventArgs args)
+        {
+            this.NavigationService.Navigate(new Uri("/LongListSelection.xaml", UriKind.Relative));
+        }
+
+        void OnTapCommand2(object sender, EventArgs args)
+        {
+            this.NavigationService.Navigate(new Uri("/LongListSelection.xaml", UriKind.Relative));
+        }
+
+
+
+        private double PreviousOpacity { get; set; }
+        void Appbar_StateChanged(object sender, ApplicationBarStateChangedEventArgs args)
+        {
+            var opacity = this.ApplicationBar.Opacity;
+            this.ApplicationBar.Opacity = args.IsMenuVisible ? 1 : PreviousOpacity;
+            this.PreviousOpacity = opacity;
+        }
+
     }
+
 }
